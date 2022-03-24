96e9017e2527e9fb264ed11699320673d17c6c08
khanhptFx13510 <khanhdudn@gmail.com>
2022-03-24 08:55:20 +0700

    Presentational and Container Components

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -1,35 +1,55 @@
 import React, { Component } from 'react';
-import { Card , CardBody , CardText , CardTitle } from 'reactstrap';
+import { Card , CardBody , CardText , CardTitle,  CardImg } from 'reactstrap';
 
 class DishDetail extends Component {
    constructor(props) {
       super(props);
       
    }
-   render() {
-      let comments
-      if(this.props.dish != undefined){
-         console.log(this.props.dish);
-            comments = this.props.dish.comments.map((comment) => {
-            return (
-               
-                  <CardBody key={comment.id}>
-                     <h3>Comments</h3>
-                     <CardText>{comment.comment}</CardText>
-                     <CardText>-- {comment.author},  
-                        {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                     </CardText>
+
+   renderSeletedDetails(dish) {
+      if(dish != undefined){
+         console.log(dish);
+         return (
+            <div className="row">
+               <Card key={dish.id} className="col-12 col-md-5 m-1">
+
+                  <CardImg src={dish.image} alt={dish.name} />
+
+                  <CardBody>
+                     <CardTitle>{dish.name}</CardTitle>
+                     <CardText>{dish.description}</CardText>
                   </CardBody>
-            
-   
-            )
-         });
+               </Card>
+               <div className="col-12 col-md-5 m-1">
+                  {dish.comments.map(comment => {
+                     return (
+                        <CardBody key={comment.id}>
+                           <h3>Comments</h3>
+                           <CardText>{comment.comment}</CardText>
+                           <CardText>-- {comment.author},  
+                              {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                           </CardText>
+                        </CardBody>
+                     )
+                  })}
+               </div>
+
+            </div>
+
+         
+
+         )
       }
+
+   }
+   render() {
+      let dish = this.renderSeletedDetails(this.props.dish)
 
 
       return (
          <div className="container">
-            {comments}
+            {dish}
          </div>
             
       )
